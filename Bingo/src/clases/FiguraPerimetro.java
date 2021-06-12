b800d1e0a665a703153442964fd2ace8af1f5b46
Alejo Trias <atriass@outlook.es>
2021-06-12 19:06:53 -0300

    Valida FiguraPerimetro

--- Bingo/src/clases/FiguraPerimetro.java
+++ Bingo/src/clases/FiguraPerimetro.java
@@ -14,16 +14,33 @@ import interfaces.IFigura;
 public class FiguraPerimetro implements IFigura {
     @Override
     public boolean cumpleFigura(Carton carton) {
-        int largoCol = carton.getColumnas();     
-        boolean puedeSeguir = true;  
+        int largoCol = carton.getColumnas();  
+        int largoFila = carton.getFilas();
+        int largo = (largoCol * largoFila) - 1; 
+        
+        boolean puedeSeguirExtremo = true;  
+        boolean puedeSeguirMedio = true;
+        
         int i = 0;
         int auxTotalCeldas = carton.getCeldas().size()-1;
-        while(i < largoCol-1){
-            if(carton.getCeldas().get(i).getBolilla() == null || carton.getCeldas().get(auxTotalCeldas).getBolilla() == null) return false;
+        
+        while(i < largoCol-1 && puedeSeguirExtremo){
+            if(carton.getCeldas().get(i).getBolilla() == null || carton.getCeldas().get(auxTotalCeldas).getBolilla() == null) puedeSeguirExtremo = false;
             i++;
             auxTotalCeldas--;
         }
-        return true;
+        
+        int j = largoFila;
+        
+        while(j < largo && puedeSeguirMedio){
+            if(carton.getCeldas().get(j).getBolilla() == null || carton.getCeldas().get(j*2-1).getBolilla() == null) puedeSeguirMedio = false;
+            j+=largoFila;
+        }
+        
+        
+        
+        return puedeSeguirExtremo && puedeSeguirMedio;
+        
     }
     
     
